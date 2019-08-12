0832add0d17ca04d7396418e13755ca8ef1dfa3f
ins21 <leonnby@gmail.com>
2019-08-12 20:38:05 +0300

    add files

---
+++ precache-manifest.0be0dc41438ed0dc545cdfffa6ac8ebd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11da9619b3f35b574d2741ff82749bf",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "6ea762303d930459c07a",
+    "url": "/chat/static/css/main.159658c4.chunk.css"
+  },
+  {
+    "revision": "049c4aa2585adf632bdc",
+    "url": "/chat/static/js/2.facabb3a.chunk.js"
+  },
+  {
+    "revision": "6ea762303d930459c07a",
+    "url": "/chat/static/js/main.8c10b616.chunk.js"
+  },
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  }
+]);
